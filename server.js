9f022797e15400eaf08531ded6ab6b19927045d4
oumaima <omaimaelbdraouy@gmail.com>
2025-02-25 22:38:34 +0000

    nice

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-app.use("/images", express.static(path.join(__dirname, 'images')));
-app.use("/css", express.static(path.join(__dirname, 'css')));
-app.use("/font", express.static(path.join(__dirname, 'font')));
-app.use("/js", express.static(path.join(__dirname, 'js')));
-app.use("/sass", express.static(path.join(__dirname, 'sass')));
+// app.use("/images", express.static(path.join(__dirname, 'images')));
+// app.use("/css", express.static(path.join(__dirname, 'css')));
+// app.use("/font", express.static(path.join(__dirname, 'font')));
+// app.use("/js", express.static(path.join(__dirname, 'js')));
+// app.use("/sass", express.static(path.join(__dirname, 'sass')));
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -42,9 +42,9 @@ app.get("/download-cv", (req, res) => {
       }
   });
 });
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 app.post("/submit-form", async (req, res) => {
   try {
     const { name, email, message } = req.body;
