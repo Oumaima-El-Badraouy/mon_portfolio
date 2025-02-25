5fff5bf13d1b69d6c425151b8d369232f82a9161
oumaima <omaimaelbdraouy@gmail.com>
2025-02-25 22:15:27 +0000

    nice

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-
+app.use("/images", express.static(path.join(__dirname, 'images')));
+app.use("/css", express.static(path.join(__dirname, 'css')));
+app.use("/font", express.static(path.join(__dirname, 'font')));
+app.use("/js", express.static(path.join(__dirname, 'js')));
+app.use("/sass", express.static(path.join(__dirname, 'sass')));
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
