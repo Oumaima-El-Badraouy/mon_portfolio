738a84e17aab99d3221aeec8f525b1dd67b7948f
oumaima <omaimaelbdraouy@gmail.com>
2025-02-25 21:38:13 +0000

    nice

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 
 // Connexion à MongoDB
 mongoose
-.connect("mongodb://localhost:27017/comments")
+.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true })
 
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
